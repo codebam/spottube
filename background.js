75661ffd9e0dea491cf30074451914499b4a2a56
Sean Behan <codebam@riseup.net>
2022-02-15 06:23:10 -0500

    cross browser compatibility, now works in firefox

--- background.js
+++ background.js
@@ -6,28 +6,33 @@ chrome.browserAction.setBadgeText({ text: "(ツ)" });
 chrome.browserAction.setBadgeBackgroundColor({ color: "#eae" });
 
 function copyToClipboard(text) {
-  const input = document.createElement("textarea");
-  input.style.position = "fixed";
-  input.style.opacity = 0;
-  input.value = text;
-  document.body.appendChild(input);
-  input.select();
-  document.execCommand("Copy");
-  document.body.removeChild(input);
+  const isFirefox = !chrome.app;
+  if (isFirefox) {
+    navigator.clipboard.writeText(text);
+  } else {
+    const input = document.createElement("textarea");
+    input.style.position = "fixed";
+    input.style.opacity = 0;
+    input.value = text;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand("Copy");
+    document.body.removeChild(input);
+  }
 }
 
-chrome.browserAction.onClicked.addListener((aTab) => {
-  chrome.tabs.getSelected(null, (tab) => {
+chrome.browserAction.onClicked.addListener(() => {
+  browser.tabs.query({ active: true }).then((tab) => {
     const key = "AIzaSyA0_xhHQX9BY9DINdzKoFpV0vJgAlj9rAw";
     const request = new Request(
       `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${encodeURIComponent(
-        tab.title
+        tab[0].title
       )}&key=${key}`
     );
     fetch(request)
       .then((response) => response.json())
       .then((json) => "https://youtu.be/" + json.items[0].id.videoId)
       .then(copyToClipboard)
-      .catch(chrome.extension.getBackgroundPage().console.log);
+      .catch(console.log);
   });
 });
